8a170dc15a896f78c796a69da4fecf5d28305bb0
gachez <briangach98@gmail.com>
2020-05-05 21:22:37 +0300

    auth init

---
+++ react-firebase-authentication/src/firebaseConfig.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCaLoLSX1NqLIMelsgDsX5ummAjWhCK4K0",
+    authDomain: "authentication-guide.firebaseapp.com",
+    databaseURL: "https://authentication-guide.firebaseio.com",
+    projectId: "authentication-guide",
+    storageBucket: "authentication-guide.appspot.com",
+    messagingSenderId: "524185603448",
+    appId: "1:524185603448:web:c4580bce73a09b40c28c8d",
+    measurementId: "G-579RCG6T59"
+  };
