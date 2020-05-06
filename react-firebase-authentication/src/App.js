0e9746a505ba5334ca9a680d101e987137f20675
gachez <briangach98@gmail.com>
2020-05-06 09:51:05 +0300

    finish home page

--- react-firebase-authentication/src/App.js
+++ react-firebase-authentication/src/App.js
@@ -6,25 +6,43 @@ import * as firebase from 'firebase/app';
 import 'firebase/auth';
 import firebaseConfig from './firebaseConfig';
 
+//initialize firebase app using the config in firebaseConfig file
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+
+//setup the providers to support access the auth library
+const firebaseAppAuth = firebaseApp.auth();
+const providers = {
+  googleProvider: new firebase.auth.GoogleAuthProvider(),
+};
+
+
 function App() {
+  const {
+    user,
+    signOut,
+    signInWithGoogle,
+  } = this.props;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header className="App-header">
+      <img src={logo} className="App-logo" alt="logo" />
+      {
+        user 
+          ? <p>Hello, {user.displayName}</p>
+          : <p>Please sign in.</p>
+      }
+      {
+        user
+          ? <button onClick={signOut}>Sign out</button>
+          : <button onClick={signInWithGoogle}>Sign in with Google</button>
+      }
+    </header>
+  </div>
   );
 }
 
-export default App;
+export default withFirebaseAuth({
+  providers,
+  firebaseAppAuth,
+})(App);
